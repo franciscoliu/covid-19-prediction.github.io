9b7e4d5ff8d172f510c6bb8c79bee93b46aed187
franciscoliu <zheyuanliu@brandeis.edu>
2020-10-30 03:37:48 -0400

    Update import json.py

--- Python API/import json.py
+++ Python API/import json.py
@@ -1,23 +1,24 @@
 import json
 from datetime import datetime
-# def writeJson():
-state_dict = {}
-with open('F:\Research Project\HTML_El\Python API\dataset.json') as file:
-    dataset = json.load(file)
+def writeJson():
+    state_dict = {}
+    with open('F:\Research Project\HTML_El\Python API\dataset.json') as file:
+        dataset = json.load(file)
 
-    for i in dataset["records"]:
-        i["fields"]["Confirmed Cases"] = int(i["fields"]["Confirmed Cases"])
-        i["fields"]["Predicted Cases"] = int(i["fields"]["Predicted Cases"])
-    for data in dataset["records"]:
-        state = data["fields"].pop("State")
-        state_dict.update(
-            {
-                state: state_dict.get(state, [])
-            }
-        )
-        state_dict[state].append(data['fields'])
-    for _, entries in state_dict.items():
-        entries.sort(key=lambda entry: entry['Date'])
+        for i in dataset["records"]:
+            i["fields"]["Confirmed Cases"] = int(i["fields"]["Confirmed Cases"])
+            i["fields"]["Predicted Cases"] = int(i["fields"]["Predicted Cases"])
+        for data in dataset["records"]:
+            state = data["fields"].pop("State")
+            state_dict.update(
+                {
+                    state: state_dict.get(state, [])
+                }
+            )
+            state_dict[state].append(data['fields'])
+        for _, entries in state_dict.items():
+            entries.sort(key=lambda entry: entry['Date'])
+    return state_dict
   
 
 def write_json(data):
@@ -25,6 +26,12 @@ def write_json(data):
         json.dump(data, outfile)
 
 def lambda_handler(event, context):
-    write_json(state_dict)
+    write_json(writeJson())
+
+
+if __name__ == '__main__':
+    print('get data from Airtable and write to json file')
+    write_json(writeJson())
+
 
 
